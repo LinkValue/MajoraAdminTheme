c6662f2cbb049de8833e0cd43c1a8b240e3079b8
OrestHk <hrynchuk.orest@gmail.com>
2016-12-12 23:40:39 +0100

    components organization + clipboard code

--- src/js/common-scripts.js
+++ src/js/common-scripts.js
@@ -26,4 +26,31 @@
 
     $('[data-toggle="tooltip"]').tooltip({ container: "body" });
     $('[data-toggle="popover"]').popover({ container: "body" });
+
+    // Menu handler
+    $("#navigation .inner-link").click(function(e){
+      e.preventDefault();
+      var id = $(this).attr('href');
+      $("#navigation .inner-link").closest('.menu-item').removeClass('is-open');
+      $("body, html").scrollTop($(id).offset().top - $(".header").outerHeight(true));
+      $(this).closest('.menu-item').addClass('is-open');
+    });
+
+    $(".fa-clipboard").click(function(){
+      var $this = $(this);
+      var clipboard = new Clipboard('.fa-clipboard', {
+        text: function(){
+          var html = $($this.attr('data-clipboard-target'));
+          return html[0].outerHTML;
+        }
+      });
+
+      $(this).prepend('<p class="clip-done">Copied&nbsp;!</p>');
+
+      setTimeout(function(){
+        $(".clip-done").fadeOut(300, function(){
+          $(this).remove();
+        });
+      }, 700);
+    });
 })($);
